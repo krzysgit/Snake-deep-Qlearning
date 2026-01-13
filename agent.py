c0f50dbab17518ead7809b139944655a71846501
Jakub Pieczonka <J.Pieczonka@student.tudelft.nl>
2026-01-13 18:09:56 +0100

    model: deep q learning neural network coded

--- agent.py
+++ agent.py
@@ -1,6 +1,5 @@
 from collections import deque
 
-import gymnasium as gym
 import numpy as np
 import torch
 import torch.nn as nn
@@ -11,17 +10,17 @@ class DQN:
             epsilon_greedy=1.0, epsilon_min=0.01,
             epsilon_decay=0.995, learning_rate=1e-3,
             max_memory_size=2000):
+        # Agent setup
         self.env = env
-        self.gamma = discount_factor
+        self.df = discount_factor
         self.epsilon = epsilon_greedy
         self.epsilon_min = epsilon_min
         self.epsilon_decay = epsilon_decay
         self.lr = learning_rate
         self.memory = deque(maxlen=max_memory_size)
-        self.state_size = env.observation_space.shape[0]
         self.action_size = env.action_space.n
-        #Torch NN
-        self.model = nn.Sequential(nn.Linear(self.state_size, 256),
+        # Torch NN setup
+        self.model = nn.Sequential(nn.Linear(env.observation_space.shape[0], 256),
                                    nn.ReLU(),
                                    nn.Linear(256, 128),
                                    nn.ReLU(),
@@ -32,4 +31,52 @@ class DQN:
         self.optimizer = torch.optim.Adam(
             self.model.parameters(), self.lr)
 
-        
+    def choose_action(self, state):
+        if np.random.rand() <= self.epsilon:
+            # Exploration
+            return np.random.choice(self.action_size)
+
+        with torch.no_grad():
+            # Exploitation
+            output = self.model(torch.tensor(state,
+                                               dtype=torch.float32))[0]
+        return torch.argmax(output).item()
+
+    def remember(self, transition):
+        self.memory.append(transition)
+
+    def _learn(self, batch_samples):
+        batch_states, batch_targets = [], []
+
+        for transition in batch_samples:
+            s, a, r, next_s, done = transition
+            with torch.no_grad():
+                if done:
+                    target = r
+                else:
+                    pred = self.model(torch.tensor(next_s,
+                                                   dtype=torch.float32))[0]
+                    target = r + self.df * pred.max()
+            target_all = self.model(torch.tensor(s,
+                                                 dtype=torch.float32))[0]
+            target_all[a] = target
+
+            batch_states.append(s.flatten())
+            batch_targets.append(target_all)
+            self._adjust_epsilon()
+
+        self.optimizer.zero_grad()
+        pred = self.model(torch.tensor(batch_states,
+                                            dtype=torch.float32))
+        loss = self.loss_fn(pred, torch.stack(batch_targets))
+        loss.backward()
+        self.optimizer.step()
+        return loss.item()
+
+    def _adjust_epsilon(self):
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+
+    def replay(self, batch_size):
+        samples = random.sample(self.memory, batch_size)
+        return self._learn(samples)
